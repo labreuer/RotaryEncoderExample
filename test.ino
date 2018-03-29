ce23cb21e3e7944488df9bd3ccf95711fdd54ce3
Luke Breuer <labreuer@gmail.com>
2018-03-29 12:17:01 -0700

    switch to Timer2, no more INT0/INT1

--- test.ino
+++ test.ino
@@ -7,18 +7,12 @@
 #define ROT_B _BV(2)
 
 void setup() {
-  // put your setup code here, to run once:
-  //pinMode(2, INPUT_PULLUP);
-  //pinMode(3, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(2), change2, CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(3), change3, CHANGE);
-
   Serial.begin(9600);
 
   DDRB |= 0x3F;
-  ICR1 = 110;
-  TCCR1A = _BV(WGM11);
-  TIMSK1 |= _BV(TOIE1);
+  OCR2A = 110;
+  TCCR2A = _BV(WGM20);
+  TIMSK2 |= _BV(TOIE2);
 
   ROT_PORT |= ROT_A | ROT_B;
   PCIFR |= _BV(PCIF1);
@@ -30,9 +24,9 @@ volatile int _v = 0;
 volatile byte _lastRot;
 volatile char _sign = 0;
 
-ISR(TIMER1_OVF_vect) {
+ISR(TIMER2_OVF_vect) {
   PORTB &= ~0x1F;
-  TCCR1B = 0;
+  TCCR2B = 0;
   _v += _sign;
 }
 
@@ -40,7 +34,7 @@ ISR(PCINT1_vect) {
   byte _rot = ROT_PIN;
   if ((_lastRot ^ _rot) & ROT_A && (_rot & ROT_B) == 0) {
     if (_rot & ROT_A) {
-      TCCR1B = 0;
+      TCCR2B = 0;
     } else {
       _sign = -1;
       setDelay();
@@ -48,7 +42,7 @@ ISR(PCINT1_vect) {
   }
   if ((_lastRot ^ _rot) & ROT_B && (_rot & ROT_A) == 0) {
     if (_rot & ROT_B) {
-      TCCR1B = 0;
+      TCCR2B = 0;
     } else {
       _sign = 1;
       setDelay();
@@ -57,38 +51,9 @@ ISR(PCINT1_vect) {
   _lastRot = _rot;
 }
 
-void change2() {
-  // only pay attention if the other pin is already low
-  if (!(PIND & _BV(3)))
-    return;
-  // if this pin high, wait for a stable low
-  if (PIND & _BV(2)) {
-    TCCR1B = 0;
-    return;
-  }
-
-  _sign = 1;
-  setDelay();
-}
-
-void change3() {
-  // only pay attention if the other pin is already low
-  if (!(PIND & _BV(2)))
-    return;
-  // if this pin high, wait for a stable low
-  if (PIND & _BV(3)) {
-    TCCR1B = 0;
-    return;
-  }
-
-  _sign = -1;
-  setDelay();
-}
-
 void setDelay() {
-  //EIMSK &= ~0x03;
-  TCNT1 = 1;
-  TCCR1B = _BV(CS11) | _BV(CS10) | _BV(WGM13) | _BV(WGM12);
+  TCNT2 = 1;
+  TCCR2B = _BV(CS22) | _BV(WGM22);
   PORTB |= _BV(4);
 }
 
