c10238587570a06d772e9847f75ab3cf0a0b0c42
Luke Breuer <labreuer@gmail.com>
2018-03-29 12:07:11 -0700

    PCINT1 and INT0/INT1 versions of rotary encoder

--- test.ino
+++ test.ino
@@ -1,65 +1,103 @@
+#define sbi(x,y) x |= _BV(y) //set bit - using bitwise OR operator 
+#define cbi(x,y) x &= ~(_BV(y)) //clear bit - using bitwise AND operator
 
+#define ROT_PORT PORTC
+#define ROT_PIN PINC
+#define ROT_A _BV(1)
+#define ROT_B _BV(2)
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), fall2, FALLING);
-  attachInterrupt(digitalPinToInterrupt(3), fall3, FALLING);
+  //pinMode(2, INPUT_PULLUP);
+  //pinMode(3, INPUT_PULLUP);
+  //attachInterrupt(digitalPinToInterrupt(2), change2, CHANGE);
+  //attachInterrupt(digitalPinToInterrupt(3), change3, CHANGE);
 
   Serial.begin(9600);
 
-  //Timer1.initialize(1000);
-  //Timer1.attachInterrupt(tim);
-  DDRB |= 0xF;
+  DDRB |= 0x3F;
+  ICR1 = 110;
+  TCCR1A = _BV(WGM11);
+  TIMSK1 |= _BV(TOIE1);
+
+  ROT_PORT |= ROT_A | ROT_B;
+  PCIFR |= _BV(PCIF1);
+  PCICR |= _BV(PCIE1);
+  PCMSK1 |= ROT_A | ROT_B;
 }
 
 volatile int _v = 0;
+volatile byte _lastRot;
+volatile char _sign = 0;
 
 ISR(TIMER1_OVF_vect) {
-  PORTB &= ~_BV(3);
-  PORTB |= _BV(2);
+  PORTB &= ~0x1F;
   TCCR1B = 0;
-  EIFR = 0x03;
-  EIMSK |= 0x03;
-  PORTB &= ~_BV(2);
+  _v += _sign;
 }
 
-void fall2() {
+ISR(PCINT1_vect) {
+  byte _rot = ROT_PIN;
+  if ((_lastRot ^ _rot) & ROT_A && (_rot & ROT_B) == 0) {
+    if (_rot & ROT_A) {
+      TCCR1B = 0;
+    } else {
+      _sign = -1;
+      setDelay();
+    }
+  }
+  if ((_lastRot ^ _rot) & ROT_B && (_rot & ROT_A) == 0) {
+    if (_rot & ROT_B) {
+      TCCR1B = 0;
+    } else {
+      _sign = 1;
+      setDelay();
+    }
+  }
+  _lastRot = _rot;
+}
+
+void change2() {
+  // only pay attention if the other pin is already low
   if (!(PIND & _BV(3)))
     return;
-  PORTB |= _BV(0);
-  _v++;
+  // if this pin high, wait for a stable low
+  if (PIND & _BV(2)) {
+    TCCR1B = 0;
+    return;
+  }
+
+  _sign = 1;
   setDelay();
-  PORTB &= ~_BV(0);
 }
 
-void fall3() {  
+void change3() {
+  // only pay attention if the other pin is already low
   if (!(PIND & _BV(2)))
     return;
-  PORTB |= _BV(1);
-  _v--;
+  // if this pin high, wait for a stable low
+  if (PIND & _BV(3)) {
+    TCCR1B = 0;
+    return;
+  }
+
+  _sign = -1;
   setDelay();
-  PORTB &= ~_BV(1);
 }
 
 void setDelay() {
-  EIMSK &= ~0x03;
-  ICR1 = 400;
-  TCCR1A = _BV(WGM11);
-  TCCR1B = _BV(CS12) | _BV(WGM13) | _BV(WGM12);
-  TIMSK1 |= _BV(TOIE1);
-  PORTB |= _BV(3);
+  //EIMSK &= ~0x03;
+  TCNT1 = 1;
+  TCCR1B = _BV(CS11) | _BV(CS10) | _BV(WGM13) | _BV(WGM12);
+  PORTB |= _BV(4);
 }
 
 volatile int _last = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
   if (_v != _last)
   {
     _last = _v;
     Serial.print(_v);
     Serial.println();
   }
-  //delay(100);
 }
